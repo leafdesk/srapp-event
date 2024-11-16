a46aeaeb9bcaf6f8dfd0fc5c7eea6e6725637811
leafdesk <lsg001008@gmail.com>
2024-11-16 15:40:02 +0900

    :hammer: [ADD] `Button` 컴포넌트 추가

---
+++ src/components/button.tsx
@@ -0,0 +1,29 @@
+type ButtonProps = {
+  onClick?: () => void
+  bgColor?: string
+  textColor?: string
+  text?: string
+}
+
+/**
+ * 버튼.
+ */
+const Button = ({
+  onClick = () => {},
+  bgColor = '#FF3296',
+  textColor = 'white',
+  text = '',
+}: ButtonProps) => {
+  return (
+    <div className="px-4 mt-10">
+      <button
+        onClick={onClick}
+        className={`w-full h-[60px] rounded-lg flex items-center justify-center bg-[${bgColor}] font-medium text-xl text-${textColor}`}
+      >
+        {text}
+      </button>
+    </div>
+  )
+}
+
+export default Button
