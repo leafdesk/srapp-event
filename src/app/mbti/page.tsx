4d8a89130ac33233eec6ed5b8c171673738a8c97
leafdesk <lsg001008@gmail.com>
2024-11-16 16:21:28 +0900

    :bug: [FIX] redirect 대신 router.push 사용 (#2)

--- src/app/mbti/page.tsx
+++ src/app/mbti/page.tsx
@@ -1,24 +1,27 @@
 'use client'
 
-import { redirect, useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import WelcomePage from './welcome-page'
 import GuidePage from './guide-page'
 import ProfilePage from './profile-page'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 
 /**
  * 사랑 긍휼 유형 테스트. (편의상 MBTI 페이지로 부름)
  */
 const MBTIPage = () => {
+  const router = useRouter()
   const searchParams = useSearchParams()
   const page = searchParams.get('page')
 
   // 허용된 페이지 params 리스트.
   const validPages = ['welcome', 'guide', 'profile', 'test']
   // params가 없거나, 허용된 페이지가 아니면 welcome 페이지로 redirect.
-  if (!page || !validPages.includes(page)) {
-    redirect('/mbti?page=welcome')
-  }
+  useEffect(() => {
+    if (!page || !validPages.includes(page)) {
+      router.push('/mbti?page=welcome')
+    }
+  }, [page, router, validPages])
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
