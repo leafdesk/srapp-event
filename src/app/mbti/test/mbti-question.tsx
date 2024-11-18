cfcfa7e2068ed29f05fefbf6014fdb9795e0d451
leafdesk <lsg001008@gmail.com>
2024-11-18 12:09:05 +0900

    :hammer: [ADD] `MBTIQuestion` 컴포넌트 추가

---
+++ src/app/mbti/test/mbti-question.tsx
@@ -0,0 +1,164 @@
+import { useEffect, useState } from 'react'
+
+type MBTIQuestionProps = {
+  question: string | React.ReactNode
+}
+
+const MBTIQuestion = ({ question }: MBTIQuestionProps) => {
+  const [selectedOption, setSelectedOption] = useState(0)
+
+  useEffect(() => console.log(selectedOption), [selectedOption])
+
+  // 라디오 버튼 선택 시 값 설정
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(parseInt(event.target.value))
+  }
+
+  return (
+    <section className="h-[175px] px-4">
+      <div className="flex items-center justify-center mb-5">
+        <span className="font-medium text-lg text-[#111]">{question}</span>
+      </div>
+      <div className="flex items-center justify-between">
+        {/* 옵션 1: 그렇다 */}
+        <label className="flex flex-col items-center cursor-pointer">
+          <input
+            type="radio"
+            name="response"
+            value="1"
+            checked={selectedOption === 1}
+            onChange={handleChange}
+            className="hidden"
+          />
+          <div
+            className={`w-[49px] h-[49px] rounded-full flex items-center justify-center ${
+              selectedOption === 1 ? 'bg-pink-500' : 'border-2 border-gray-300'
+            }`}
+          >
+            <span
+              className={`${
+                selectedOption === 1 ? 'text-white' : 'text-transparent'
+              } text-xs`}
+            >
+              ✔️
+            </span>
+          </div>
+          <span className="mt-2 text-gray-700 text-sm h-10 text-center">
+            그렇다
+          </span>
+        </label>
+
+        {/* 옵션 2 */}
+        <label className="flex flex-col items-center cursor-pointer">
+          <input
+            type="radio"
+            name="response"
+            value="2"
+            checked={selectedOption === 2}
+            onChange={handleChange}
+            className="hidden"
+          />
+          <div
+            className={`w-[35px] h-[35px] rounded-full flex items-center justify-center ${
+              selectedOption === 2 ? 'bg-pink-500' : 'border-2 border-gray-300'
+            }`}
+          >
+            <span
+              className={`${
+                selectedOption === 2 ? 'text-white' : 'text-transparent'
+              } text-xs`}
+            >
+              ✔️
+            </span>
+          </div>
+          <span className="mt-2 text-transparent text-sm h-10"> </span>
+        </label>
+
+        {/* 옵션 3: 보통 */}
+        <label className="flex flex-col items-center cursor-pointer">
+          <input
+            type="radio"
+            name="response"
+            value="3"
+            checked={selectedOption === 3}
+            onChange={handleChange}
+            className="hidden"
+          />
+          <div
+            className={`w-[27px] h-[27px] rounded-full flex items-center justify-center ${
+              selectedOption === 3 ? 'bg-pink-500' : 'border-2 border-gray-300'
+            }`}
+          >
+            <span
+              className={`${
+                selectedOption === 3 ? 'text-white' : 'text-transparent'
+              } text-xs`}
+            >
+              ✔️
+            </span>
+          </div>
+          <span className="mt-2 pt-2.5 text-gray-700 text-sm h-10 text-center">
+            보통
+          </span>
+        </label>
+
+        {/* 옵션 4 */}
+        <label className="flex flex-col items-center cursor-pointer">
+          <input
+            type="radio"
+            name="response"
+            value="4"
+            checked={selectedOption === 4}
+            onChange={handleChange}
+            className="hidden"
+          />
+          <div
+            className={`w-[35px] h-[35px] rounded-full flex items-center justify-center ${
+              selectedOption === 4 ? 'bg-pink-500' : 'border-2 border-gray-300'
+            }`}
+          >
+            <span
+              className={`${
+                selectedOption === 4 ? 'text-white' : 'text-transparent'
+              } text-xs`}
+            >
+              ✔️
+            </span>
+          </div>
+          <span className="mt-2 text-transparent text-sm h-10"> </span>
+        </label>
+
+        {/* 옵션 5: 그렇지 않다 */}
+        <label className="flex flex-col items-center cursor-pointer">
+          <input
+            type="radio"
+            name="response"
+            value="5"
+            checked={selectedOption === 5}
+            onChange={handleChange}
+            className="hidden"
+          />
+          <div
+            className={`w-[49px] h-[49px] rounded-full flex items-center justify-center ${
+              selectedOption === 5 ? 'bg-pink-500' : 'border-2 border-gray-300'
+            }`}
+          >
+            <span
+              className={`${
+                selectedOption === 5 ? 'text-white' : 'text-transparent'
+              } text-xs`}
+            >
+              ✔️
+            </span>
+          </div>
+          <span className="mt-2 text-gray-700 text-sm h-10 text-center">
+            그렇지
+            <br /> 않다
+          </span>
+        </label>
+      </div>
+    </section>
+  )
+}
+
+export default MBTIQuestion
