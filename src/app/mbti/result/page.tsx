b5851cbfa97f664d64efd259b07a3c13f2339f93
leafdesk <lsg001008@gmail.com>
2024-11-19 18:50:11 +0900

    :hammer: [UPDATE] 결과 페이지 MBTI, 제목, 상태 바, 설명 추가 (#1)

--- src/app/mbti/result/page.tsx
+++ src/app/mbti/result/page.tsx
@@ -3,8 +3,15 @@
 import React from 'react'
 import { useAtom } from 'jotai'
 import { scoresAtom, finalResultAtom } from '@/app/mbti/store'
+import mbtiTypes from './mbti-types'
 
-const Field = ({ labelLeft, labelRight, score, color }: any) => {
+const Field = ({
+  labelLeft,
+  labelRight,
+  scoreLeft,
+  scoreRight,
+  color,
+}: any) => {
   const parseLabel = (label: string) => {
     const [kr, en] = label.split(' ') // 공백을 기준으로 한국어와 영어 분리
     return { kr, en }
@@ -19,19 +26,25 @@ const Field = ({ labelLeft, labelRight, score, color }: any) => {
       <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden mb-2">
         <div
           className="h-2 rounded-full"
-          style={{ width: `${score}%`, backgroundColor: color }}
+          style={{ width: `${scoreLeft}%`, backgroundColor: color }}
         />
       </div>
 
       {/* 라벨 */}
       <div className="flex items-center justify-between">
         <div>
-          <span className="font-medium">{krLeft}</span>{' '}
-          <span className="font-normal">{enLeft}</span>
+          <div>
+            <span className="font-medium">{krLeft}</span>{' '}
+            <span className="font-normal">{enLeft}</span>
+          </div>
+          <span>{scoreLeft}%</span>
         </div>
         <div>
-          <span className="font-medium">{krRight}</span>{' '}
-          <span className="font-normal">{enRight}</span>
+          <div>
+            <span className="font-medium">{krRight}</span>{' '}
+            <span className="font-normal">{enRight}</span>
+          </div>
+          <span>{scoreRight}%</span>
         </div>
       </div>
     </div>
@@ -43,6 +56,19 @@ const ResultPage = () => {
   const [finalResult] = useAtom(finalResultAtom)
 
   console.log('결과 페이지: ', scores, finalResult)
+
+  // finalResult에서 MBTI 타입을 가져옵니다.
+  const mbtiType = finalResult
+    ? Object.values(finalResult.result).join('')
+    : '결과 없음'
+
+  // mbtiTypes에서 해당하는 정보를 검색
+  const mbtiInfo = mbtiTypes.find((type) => type.id === mbtiType)
+  const title = mbtiInfo ? mbtiInfo.title : '결과 없음'
+  const description = mbtiInfo ? mbtiInfo.description : '설명이 없습니다.'
+
+  // 각 필드에 대한 퍼센트 정보 가져오기
+  const percentages = finalResult ? finalResult.percentages : {}
 
   return (
     <>
@@ -58,12 +84,12 @@ const ResultPage = () => {
 
       {/* MBTI 타입 */}
       <strong className="block w-full px-4 mt-5 mb-2.5 text-[#333] font-medium text-base">
-        AVIE
+        {mbtiType}
       </strong>
 
       {/* MBTI 타입 제목 */}
       <h3 className="block w-full px-4 text-[#222] font-semibold text-[22px] mb-5">
-        쨍쩅히 비취는 사랑의 빛
+        {title}
       </h3>
 
       {/* 나의 세부 항목 */}
@@ -76,31 +102,38 @@ const ResultPage = () => {
           <Field
             labelLeft="언어형 Verbal"
             labelRight="행동형 Decisive"
-            color="#4fa8f9"
+            scoreLeft={percentages['V'] ? percentages['V'].percentage : 0}
+            scoreRight={percentages['D'] ? percentages['D'].percentage : 0}
+            color="#5EC6E8"
+          />
+          <Field
+            labelLeft="적극형 Active"
+            labelRight="반응형 Reactive"
+            scoreLeft={percentages['A'] ? percentages['A'].percentage : 0}
+            scoreRight={percentages['R'] ? percentages['R'].percentage : 0}
+            color="#ECC369"
+          />
+          <Field
+            labelLeft="강렬형 Instense"
+            labelRight="담백형 Simple"
+            scoreLeft={percentages['I'] ? percentages['I'].percentage : 0}
+            scoreRight={percentages['S'] ? percentages['S'].percentage : 0}
+            color="#F29194"
+          />
+          <Field
+            labelLeft="기억형 Retentive"
+            labelRight="미래형 Visionary"
+            scoreLeft={percentages['E'] ? percentages['E'].percentage : 0}
+            scoreRight={percentages['O'] ? percentages['O'].percentage : 0}
+            color="#6BD0A5"
           />
         </div>
       </div>
+
+      {/* MBTI 타입 설명 */}
+      <p className="mt-4 text-[#555]">{description}</p>
     </>
   )
 }
 
 export default ResultPage
-
-// const label = [
-//   [
-//     { kr: '언어형', en: 'Verbal' },
-//     { kr: '행동형', en: 'Decisive' },
-//   ],
-//   [
-//     { kr: '적극형', en: 'Active' },
-//     { kr: '반응형', en: 'Reactive' },
-//   ],
-//   [
-//     { kr: '강렬형', en: 'Instense' },
-//     { kr: '담백형', en: 'Simple' },
-//   ],
-//   [
-//     { kr: '기억형', en: 'Retentive' },
-//     { kr: '미래형', en: 'Visionary' },
-//   ],
-// ]
